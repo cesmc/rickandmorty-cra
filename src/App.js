6c66a7c7773f837b18f1bfd730fba0f1e29824c8
cesmc <xccuevas@gmail.com>
2022-04-22 21:28:21 -0500

    feat(context): created usecontext

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header/Header';
 import Characters from './components/Characters/Characters';
+import ThemeContext from './context/ThemeContext';
 import './App.css';
 
 function App() {
+  const [theme, updateTheme] = useState('theme-light');
+
   return (
-    <div className="App">
-      <Header/>
-      <Characters/>
-    </div>
+    <ThemeContext.Provider value={{ theme, updateTheme }}>
+      <div className={ 'App ' + theme }>
+        <Header/>
+        <Characters/>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import ThemeContext from '../../context/ThemeContext';
 import './style.css';
 
 const Header = () => {
   const [darkMode, setDarkmode] = useState(false);
+  const { theme, updateTheme } = useContext(ThemeContext);
 
   const handleClick = () => {
     setDarkmode(!darkMode);
+    theme === 'theme-light' ? updateTheme('theme-dark') : updateTheme('theme-light');
   }
 
   return (
     <div className='Header_container'>
      <div>Rick and Morty API</div>
-     <button type='button' onClick={handleClick}>{!darkMode ? 'Dark Mode' : 'Light Mode'}</button>
+     <button type='toggel-button' onClick={handleClick}>{!darkMode ? 'Dark Mode' : 'Light Mode'}</button>
     </div>
   )
 }
